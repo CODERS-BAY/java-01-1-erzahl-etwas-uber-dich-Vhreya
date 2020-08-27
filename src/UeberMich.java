5fe1185348e5baa26c21f6e2de3a747ef722c74e
Vanessa <va.au@gmx.at>
2020-08-27 09:35:47 +0200

    Add UeberMich class

---
+++ src/UeberMich.java
@@ -0,0 +1,12 @@
+public class UeberMich {
+    public static void main(String[] args) {
+
+        int age = 26;
+        String name = "Vanessa";
+        char gender = 'w';
+        String lastName = "Tiziani";
+        String birthday = "15.11.1993";
+        double grades = 3.4;
+
+    }
+}
