ddf1efa6744379f605377a63a9d6afc7cf21be3a
Vanessa <va.au@gmx.at>
2020-08-27 10:01:13 +0200

    finished

--- src/UeberMich.java
+++ src/UeberMich.java
@@ -2,12 +2,14 @@ public class UeberMich {
     public static void main(String[] args) {
 
         int age = 26;
-        String name = "Vanessa";
-        char gender = 'w';
+        String firstName = "Vanessa";
         String lastName = "Tiziani";
+        char gender = 'W';
         String birthday = "15.11.1993";
-        double grades = 3.4;
-        int a = 4;
+        double averageGrades = 3.4;
+        boolean married = false;
+
+        System.out.println("Hello, my name is " + firstName + " " + lastName + ". I'm born on " + birthday + " and i'm " + gender + ". My Grades average is " + averageGrades + " and married= " + married + ".");
 
     }
 }
